4bb1d851c7e2ebad0de45e50437f5a985a7ce407
Peter Amstutz <peter.amstutz@curii.com>
2020-03-12 15:30:25 -0400

    16218: Check ruby version
    Arvados-DCO-1.1-Signed-off-by: Peter Amstutz <peter.amstutz@curii.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.4')
+  raise "The SSO server must be run on Ruby version 2.3, version 2.4+ is not supported"
+end
+
 # Initialize the rails application
 CfiOauthProvider::Application.initialize!
